3408a09c29c01ca2180635eb736bcbd2c4d7ec21
Fabienne128 <59924685+Fabienne128@users.noreply.github.com>
2020-05-19 13:40:21 +0200

    Update - Link to css

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,14 +5,15 @@ function myMove() {
     function frame() {
       if (pos == 150) {
         clearInterval(id);
-        function setzeCSSLink() {
+        /*if (!document.getElementById)*/ document.write('<link rel="stylesheet" type="text/css" href="style/newstyle.css">');
+        /*function setzeCSSLink() {
             var CSSlink = document.createElement('style/newstyle.css');
                 CSSlink.rel = "stylesheet";
                 CSSlink.href = "style/newstyle.css";
                 CSSlink.type = "text/css";
                 
-                /*document.getElementsByTagName('head')[0].appendChild(CSSlink)*/
-            }
+                /*document.getElementsByTagName('head')[0].appendChild(CSSlink)
+            }*/
         /*document.getElementById("heading").style.color = "blue";*/
       } else {
         pos++; 
